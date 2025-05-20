4c365f7f37065fc6408439783b9380f9c0d17590
Maha Abo Assi <eng.mahaab96@gmail.com>
2025-05-20 16:08:52 +0400

    fix arrow in heading

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -2,14 +2,9 @@ const Heading = ({title, subTitle, isCenter, isWithArrows})=>{
     const arrow = <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" fill="none">
     <path d="M0.589966 10.59L5.16997 6L0.589966 1.41L1.99997 0L7.99997 6L1.99997 12L0.589966 10.59Z" fill="white"/>
     </svg>
-    const lastSlider = ()=>{
-        document.querySelector(".swiper-button-prev").click()
-        
-    }
-    const nextSlider = ()=>{
-        document.querySelector(".swiper-button-next").click()
-        
-    }
+    const lastSlider = ()=>{document.querySelector("#rooms .swiper-button-prev").click()}
+    const nextSlider = ()=>{document.querySelector("#rooms .swiper-button-next").click()}
+    
     return<div  className={`heading ${isCenter?"center-heading":""}`}>
         <div className="flex items-center gap-2 sub-title">
             <div className="align"></div>
