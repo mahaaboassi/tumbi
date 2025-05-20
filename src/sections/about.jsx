675c5a8d3cce16e7e9614a4d5f4009d827bc03f2
Maha Abo Assi <eng.mahaab96@gmail.com>
2025-05-20 11:16:19 +0400

    fix /#room to #room

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -8,7 +8,7 @@ const About = ()=>{
         <div className="w-full flex flex-col gap-10 justify-center ">
             <Heading subTitle={"WELCOME TO HOTEL"} title={"Get to Know Our Luxury Hotels"} />
             <p>At TUMBI Hotels, we are committed to delivering an exceptional experience that  nurtures the mind, body, and soul. We provide personalized services, wellness driven  activities while placing you in the lap of luxury. Creating a sanctuary where they can  reconnect with themselves, embrace nature, and cultivate inner peace. Our mission is  to create a space that promotes balance, vitality, and well-being, allowing our guests to  feel refreshed, grounded, and rejuvenated.”</p>
-            <a href="/#rooms" className="!px-10 email-button">let's start</a>
+            <a href="#rooms" className="!px-10 email-button">let's start</a>
         </div>
     </div>)
 }
